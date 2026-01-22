cd1774798a21e359a54093e497388cbe174c4423
JustinHowell22 <146649001+JustinHowell22@users.noreply.github.com>
2026-01-22 17:27:20 -0500

    Refactor vite.config.js to use arrow function

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,22 @@
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: "assets/ofg-premium.js",
-        chunkFileNames: "assets/ofg-chunk.js",
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name && assetInfo.name.endsWith(".css")) {
-            return "assets/ofg-premium.css";
-          }
-          return "assets/[name][extname]";
+export default defineConfig(() => {
+  return {
+    plugins: [react()],
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: "assets/ofg-premium.js",
+          chunkFileNames: "assets/ofg-chunk.js",
+          assetFileNames: ({ name }) => {
+            if (name && name.endsWith(".css")) {
+              return "assets/ofg-premium.css";
+            }
+            return "assets/[name][extname]";
+          },
         },
       },
     },
-  },
+  };
 });
