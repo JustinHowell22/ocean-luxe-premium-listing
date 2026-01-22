d82d5b562094421f7e0f713e2b92ee6fa61e97d1
JustinHowell22 <146649001+JustinHowell22@users.noreply.github.com>
2026-01-22 17:43:38 -0500

    Fix quotes in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       output: {
-        entryFileNames: "assets/ofg-premium.js",
-        chunkFileNames: "assets/ofg-chunk.js",
+        entryFileNames: 'assets/ofg-premium.js',
+        chunkFileNames: 'assets/ofg-chunk.js',
         assetFileNames: (assetInfo) => {
-          if (assetInfo.name && assetInfo.name.endsWith(".css")) {
-            return "assets/ofg-premium.css";
+          if (assetInfo.name && assetInfo.name.endsWith('.css')) {
+            return 'assets/ofg-premium.css'
           }
-          return "assets/[name][extname]";
-        },
-      },
-    },
-  },
-});
+          return 'assets/[name][extname]'
+        }
+      }
+    }
+  }
+})
